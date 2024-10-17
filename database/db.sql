e12f0df36526deee5b179c18fd9da5edc5b67407
MenakaMohanraj <menakacse029@gmail.com>
2024-10-17 09:50:23 +0530

    Add files via upload

---
+++ database/db.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE teams_management
+Use teams_management 
+CREATE TABLE teams (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    team_name VARCHAR(255) NOT NULL,
+    team_size INT NOT NULL,
+    year INT NOT NULL,
+    department VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE team_members (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    team_id INT NOT NULL,
+    member_name VARCHAR(255) NOT NULL,
+    roll_no VARCHAR(50) NOT NULL,
+    member_role VARCHAR(50) NOT NULL,
+    member_email VARCHAR(255) NOT NULL,
+    member_phone VARCHAR(15) NOT NULL,
+    FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE
+);
+
+CREATE TABLE submissions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    media VARCHAR(255),
+    file VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
